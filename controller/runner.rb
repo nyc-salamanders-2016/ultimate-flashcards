dd43d15d54137fb833c1ba7e2ae3ab9a7e7cfb37
Moin Uddin  & Shawn Paik <pair+legitcoder.sorfresh@devbootcamp.com>
2016-08-12 17:07:40 -0400

    finish view

--- controller/runner.rb
+++ controller/runner.rb
@@ -1,6 +1,7 @@
 require_relative '../model/flash_card'
 require_relative '../model/flash_deck'
 require_relative '../model/parser'
+require_relative '../view/print_pretty'
 require 'pry'
 
 class FlashRunner
@@ -8,15 +9,26 @@ class FlashRunner
 
   def initialize
     @game = Game.new
-    #@view = View.new
+    @view = View.new
      @file = ARGV[0]
+     @deck = @game.read(@file)
     run
   end
 
   def run
-    if ARGV.any?
-        @game.read(@file)
+    i= 0
+    @deck.each do |card|
+      puts card.question
+      input = @view.user_input
+      if input == card.answer
+        @view.print_correct
+        i += 1
+        else
+          @view.print_wrong
+      end
+      puts
     end
+   @view.correct_count(@deck, i)
   end
 end
 
