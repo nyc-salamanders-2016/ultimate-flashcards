da1af54dde61e0b97a301cce22e2a69ebaf8600b
Edwin Carbajal & Henri El Daher <pair+edwincarbajal.heldaher@devbootcamp.com>
2016-08-12 12:14:03 -0400

    made some changes to runner

--- controller/runner.rb
+++ controller/runner.rb
@@ -2,28 +2,21 @@ require_relative '../model/flash_card'
 require_relative '../model/flash_deck'
 
 class FlashRunner
-  attr_reader :card, :game
+  attr_reader :card, :game, :file
 
   def initialize
     @game = Game.new
     @view = View.new
-    run
+    @file = ARGV[0]
   end
 
   def run
-    @nighthawk = 'nighthawk_flashcard_data.txt'
-    @otter = 'otter_flashcard_data.txt'
-    @raccoon = 'raccoon_flashcard_data.txt'
+    # @nighthawk = 'nighthawk_flashcard_data.txt'
+    # @otter = 'otter_flashcard_data.txt'
+    # @raccoon = 'raccoon_flashcard_data.txt'
 
     if ARGV.any?
-      if ARGV[0] == @nighthawk
-        @game.read(@nighthawk)
-      elsif ARGV[0] == @otter
-        @game.read(@otter)
-      elsif ARGV[0] == @raccoon
-        @game.read(@raccoon)
-      end
-
+      @game.read(@file)
     end
 
   # End run below
