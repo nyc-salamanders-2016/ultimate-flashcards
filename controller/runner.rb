05790bcd69fb6736ebeb9fc87422ac64435a2a47
Moin Uddin  & Shawn Paik <pair+legitcoder.sorfresh@devbootcamp.com>
2016-08-12 13:49:18 -0400

    Refactor

--- controller/runner.rb
+++ controller/runner.rb
@@ -1,25 +1,21 @@
 require_relative '../model/flash_card'
 require_relative '../model/flash_deck'
 require_relative '../model/parser'
+require 'pry'
 
 class FlashRunner
   attr_reader :card, :game
 
   def initialize
     @game = Game.new
-    @view = View.new
-    # @file = ARGV[0]
+    #@view = View.new
+     @file = ARGV[0]
+    run
   end
 
   def run
-    @nighthawk = '../nighthawk_flashcard_data.txt'
-    @otter = 'otter_flashcard_data.txt'
-    @raccoon = 'raccoon_flashcard_data.txt'
-
     if ARGV.any?
-      if ARGV[0] == @nighthawk
-        @game.read(@nighthawk)
-      end
+        @game.read(@file)
     end
 
   # End run below
